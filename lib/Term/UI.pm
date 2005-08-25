1eb5a9381b6a1d949aedd05827f3b92ea229628c
Jos Boumans <kane@cpan.org>
2005-08-25 12:27:15 +0000

    Import of KANE/Term-UI-0.12 from CPAN.
    gitpan-cpan-distribution: Term-UI
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         KANE/Term-UI-0.12.tar.gz
    gitpan-cpan-author:       KANE
    gitpan-cpan-maturity:     released

--- lib/Term/UI.pm
+++ lib/Term/UI.pm
@@ -11,7 +11,7 @@ use strict;
 BEGIN {
     use vars        qw[$VERSION $AUTOREPLY $VERBOSE $INVALID];
     $VERBOSE    =   1;
-    $VERSION    =   '0.11';
+    $VERSION    =   '0.12';
     $INVALID    =   loc('Invalid selection, please try again: ');
 }
 
